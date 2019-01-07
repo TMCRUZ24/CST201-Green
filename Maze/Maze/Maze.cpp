6dcd0b8462669dc99eaf483af903cb3d75f95c6a
TMCRUZ24 <32321720+TMCRUZ24@users.noreply.github.com>
2019-01-06 17:10:55 -0800

    Add files via upload
    Tyson Cruz Maze Project - Week 1

--- Maze/Maze/Maze.cpp
+++ Maze/Maze/Maze.cpp
@@ -9,6 +9,8 @@ This is my own work.
 This application will read data from a file and create a 2-dimensional maze map consisting of 3x3 squares.
 */
 
+
+/////////////////////////////////////Header files and class libraries////////////////////////////////////////
 #include "pch.h"
 #include <iostream>
 #include <fstream>
@@ -37,6 +39,7 @@ int main()
 
 //////////////////////////////////////////FUNCTION DEFINITIONS///////////////////////////////////////////////
 
+//createMaze function creates a maze grid and populates the grid with tiles specified from the text file
 vector<vector<vector<string> > > createMaze(void) {
 
 	ifstream fileIn;
@@ -47,83 +50,63 @@ vector<vector<vector<string> > > createMaze(void) {
 	vector<string> openTile = getOpenSquare();
 	vector<vector<vector<string> > > mazeGrid;
 
-	//numOfColumns = 200;
-	//numOfRows = 200;
-
+	//Open text file. If file doesn't open, throw error message
 	fileIn.open("file.in.txt");
 	if (!fileIn) {
 		cerr << "Unable to start file.in.txt";
 		exit(1);
 	}
 
+	//Initialize variables with starting values from the file
 	fileIn >> xAxis >> yAxis;
 	fileIn >> startX >> startY;
 	fileIn >> finishX >> finishY;
 
+	//Stub used to declare the size of the maze grid
 	cout << "Grid size: (" << xAxis << ", " << yAxis << ")." << endl;
 
+	//"If" statement verifies that the coordinates don't go beyond the grid size
 	if (startX > xAxis || finishX > xAxis || startY > yAxis || finishY > yAxis || 
 			xAxis < 0 || yAxis < 0 || 
 			startX < 0 || startY < 0 || finishX < 0 || finishY < 0){
-		cout << "Number of columns: " << xAxis << endl;
-		cout << "Number of rows: " << yAxis << endl;
-		cout << "Start X/T :" << startX << "  " << startY << endl;
-		cout << "Finish X/Y :" << finishX << "  " << finishY << "  " << endl;
 
 		cout << "Out of Bounds Error: Check that the start and ";
 		cout << "finish coordinates are not beyond of the size of the maze." << endl;
 	}
 
+	//Resize the grid using the variables initialized from the file
 	mazeGrid.resize(yAxis);
-	cout << "Number of rows set: " << yAxis << endl;
-
 	for (int i = 0; i < yAxis; i++) {
 		mazeGrid[i].resize(xAxis);
 	}
-	cout << "Number of columns set: " << xAxis << endl;
 
+	//Initialize the grid with all "open space" tiles
 	for (int i = 0; i < mazeGrid.size(); i++){
 		for (int j = 0; j < mazeGrid[i].size(); j++) {
 			mazeGrid[i][j] = openTile;
 		}
 	}
-	cout << "Open tiles initialized." << endl;
 
+	//Set the coordinates to place the "Start" and "Finish" tiles
 	mazeGrid[startX][startY] = startTile;
-	cout << "Start tile set: (" << startX << ", " << startY << ")." << endl;
 	mazeGrid[finishX][finishY] = finishTile;
-	cout << "Finish tile set: (" << finishX << ", " << finishY << ")." << endl;
 
-	cout << endl;
-	cout << "Grid size: (" << xAxis << ", " << yAxis << ")." << endl;
-
-	do{
+	//Use the rest of the text file as coordinates to place "Blocked" tiles
+	while (!fileIn.eof()){
 	fileIn >> blockX >> blockY;
-
-		cout << "Block (" << blockX << " , " << blockY << ") set." << endl;
 
 		if (blockX < 0 || blockY < 0 || blockX > xAxis || blockY > yAxis) {
 			cout << "Out of Bounds Error: coordinates are outside the scope of the grid" << endl;
 		}
 		mazeGrid[blockY][blockX] = blockTile;
-		
-	} while (!fileIn.eof());
+	}
 
-
-	cout << "Blocked tiles initialized." << endl;
-
+	//End of "createMaze" function, returns the maze to the "main" method
 	return mazeGrid;
 }
 
-
+//printSquare function displays the maze grid in the proper format
 void printSquare(vector<vector<vector<string> > > &row){
-
-			//cout << row.size() << endl;
-			//cout << row[0].size() << endl;
-			//cout << row[0][0][0] << row[0][1][0] << row[0][2][0] << row[0][3][0] << endl; until the end of xAxis
-			//cout << row[1][0][1] << row[1][1][1] << row[1][2][1] << row[1][3][1] << endl;
-			//cout << row[2][0][2] << row[2][1][2] << row[2][2][2] << row[2][3][2] << endl;
-			//keep going down until the end of yAxis
 
 			for (int i = 0; i < row.size(); i++) {
 				for (int j = 0; j < row[i].size(); j++) {
@@ -139,24 +122,9 @@ void printSquare(vector<vector<vector<string> > > &row){
 				}
 				cout << endl;
 			}
-			/*cout << endl;
-			for (int i = 0; i < row.size(); i++) {
-				for (int j = 0; j < row[i].size(); j++) {
-					cout << row[i][j][1];
-				}
-				cout << endl;
-			}					
-			cout << endl;
-			for (int i = 0; i < row.size(); i++) {
-				for (int j = 0; j < row[i].size(); j++) {
-					cout << row[i][j][2];
-				}
-				cout << endl;
-			}
-			cout << endl;
-			*/	
 }
 
+//creates the tile for the "Start" position
 vector<string> getStartSquare(){
 
 	vector<string> newSquare =
@@ -167,6 +135,7 @@ vector<string> getStartSquare(){
 	return newSquare;
 }
 
+//creates the tile for the "Finish" position
 vector<string> getFinishSquare() {
 
 	vector<string> newSquare =
@@ -177,6 +146,7 @@ vector<string> getFinishSquare() {
 	return newSquare;
 }
 
+//creates the tile for the "Blocked" positions
 vector<string> getBlockSquare() {
 
 	vector<string> newSquare =
@@ -187,6 +157,7 @@ vector<string> getBlockSquare() {
 	return newSquare;
 }
 
+//creates the tile for the "Open" positions
 vector<string> getOpenSquare() {
 
 	vector<string> newSquare =
